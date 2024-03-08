147f6818c1ce5e0600ad2d60b90528bb519716f6
Ekaterina-Kozlova28 <ekaterinakozlova0@gmail.com>
2024-03-08 22:12:22 +0300

    Модернизация кнопки навигации

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,6 @@ const menuBtn = document.querySelector('.main-nav__toggle');
 const menu = document.querySelector('.main-nav__list');
 
 menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('is-active');
-  menu.classList.toggle('is-active');
+  menuBtn.classList.toggle('main-nav__toggle--active');
+  menu.classList.toggle('main-nav__list--active');
 })
