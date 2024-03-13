412f66dff50b479be456c676f109251e387d312e
Ekaterina-Kozlova28 <ekaterinakozlova0@gmail.com>
2024-03-13 20:23:22 +0300

    Добавляет меню без JS в мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,9 @@
 const menuBtn = document.querySelector('.main-nav__toggle');
 const menu = document.querySelector('.main-nav__list');
 
+menuBtn.classList.remove('main-nav__toggle--nojs');
+menu.classList.remove('main-nav__list--nojs');
+
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('main-nav__toggle--active');
   menu.classList.toggle('main-nav__list--active');
