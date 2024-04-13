8ee3578cf62d3d9cf9b461d0fd900a37cc89a153
Keks <keks@htmlacademy.ru>
2024-04-13 21:21:29 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/script.js
@@ -0,0 +1,1 @@
+const menuBtn=document.querySelector(".main-nav__toggle"),menu=document.querySelector(".main-nav__list");menuBtn.classList.remove("main-nav__toggle--nojs"),menu.classList.remove("main-nav__list--nojs"),menuBtn.addEventListener("click",(()=>{menuBtn.classList.toggle("main-nav__toggle--active"),menu.classList.toggle("main-nav__list--active")}));
